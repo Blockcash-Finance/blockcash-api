e6cc3f93561297c5e987c6fa7d1c8795cfcafe89
Rahu <rahunandan@gmail.com>
2021-11-15 16:01:01 +0800

    add to router

--- src/router.js
+++ src/router.js
@@ -4,17 +4,16 @@ const Router = require("koa-router");
 const router = new Router();
 
 const noop = require("./api/noop");
-/* const stats = require('./api/stats');
-const supply = require('./api/supply');
-const price = require('./api/price');
+const stats = require("./api/stats");
+const price = require("./api/price");
+/* const supply = require('./api/supply');
 const gov = require('./api/stats/gov');
 const cmc = require('./api/cmc');
 const tvl = require('./api/tvl');
 const multichainVaults = require('./api/vaults');
 const { bifibuyback } = require('./api/stats/bifibuyback/index');
 
-router.get('/apy', stats.apy);
-router.get('/apy/breakdown', stats.apyBreakdowns);
+
 router.get('/bifibuyback', bifibuyback);
 
 router.get('/tvl', tvl.vaultTvl);
@@ -27,10 +26,13 @@ router.get('/supply/circulating', supply.circulating);
 router.get('/earnings', gov.earnings);
 router.get('/holders', gov.holderCount);
 
-router.get('/lps', price.lpsPrices);
-router.get('/prices', price.tokenPrices);
 
 router.get('/vaults', multichainVaults.multichainVaults); */
+
+router.get("/apy", stats.apy);
+router.get("/apy/breakdown", stats.apyBreakdowns);
+router.get("/lps", price.lpsPrices);
+router.get("/prices", price.tokenPrices);
 
 router.get("/", noop);
 
